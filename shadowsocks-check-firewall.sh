b25f524e35eef38e1e45d0fce1b12e2dc8df4810
doobom <rovecat@gmail.com>
2015-09-07 13:22:21 +0800

    Update shadowsocks-check-firewall.sh

--- shadowsocks-check-firewall.sh
+++ shadowsocks-check-firewall.sh
@@ -154,12 +154,12 @@ function download_files(){
 	if [ -s shadowsocks ]; then
 	    echo "shadowsocks [found]"
 	    cp -f ./shadowsocks /etc/init.d/shadowsocks
-        elif [ ! wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks -O ./shadowsocks ] && [ cp -f ./shadowsocks /etc/init.d/shadowsocks ]; then
+        elif ! [ wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks -O ./shadowsocks && cp -f ./shadowsocks /etc/init.d/shadowsocks ]; then
             echo "Failed to download shadowsocks chkconfig file!"
             exit 1
         fi
     else
-        if [ ! wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-debian -O ./shadowsocks ] && [ cp -f ./shadowsocks /etc/init.d/shadowsocks ]; then
+        if ! [ wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-debian -O ./shadowsocks && cp -f ./shadowsocks /etc/init.d/shadowsocks ]; then
             echo "Failed to download shadowsocks chkconfig file!"
             exit 1
         fi
